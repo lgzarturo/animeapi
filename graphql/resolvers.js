806f89c28e0e547b09c61c7085c4204b8978f390
Arturo López <lgzarturo@gmail.com>
2021-03-16 19:34:56 -0500

    Guardar datos del personaje

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,34 @@
+const Character = require('../models/Character')
 const {characters} = require('../data/db')
 
 /** Resolver */
 module.exports = {
   Query: {
-    getCharacters: (_, {input}, ctx) => {
-      console.log({input, ctx})
-      return characters.filter(character => character.power === input.power)
-    },
+    getCharacters: () => characters,
     getCharacter: () => { 
       const item = Math.floor(Math.random() * characters.length)
       return characters[item]
     },
     getPowers: () => characters
+  },
+  Mutation: {
+    newCharacter: async (_, {input}) => {
+      // Validar los datos del input
+      const {fullname, alias} = input
+      const existCharacter = await Character.findOne({fullname})
+      console.log(existCharacter)
+      if (existCharacter) {
+        throw new Error('El personaje ya existe en la base de datos')
+      }
+
+      try {
+        const character = new Character(input)
+        character.save()
+        return character
+      } catch(error) {
+        console.log(error)
+        throw new Error('No se pudo guardar la informacion')
+      }    
+    }
   }
 }
--- graphql/schema.js
+++ graphql/schema.js
@@ -3,17 +3,26 @@ const { gql } = require('apollo-server')
 /** Schema */
 module.exports = gql`
   type Character {
-    name: String
+    id: ID
+    fullname: String
+    alias: String
+    race: String
+    createdAt: String
   }
   type Power {
     power: String
   }
   input CharacterInput {
-    power: String 
+    fullname: String
+    alias: String
+    race: String
   }
   type Query {
-    getCharacters(input: CharacterInput!): [Character]
+    getCharacters: [Character]
     getCharacter: Character
     getPowers: [Power]
+  }
+  type Mutation {
+    newCharacter(input: CharacterInput!): Character
   }
 `
